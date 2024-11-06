ede11d5d917b84ce32fa7bf19b6532d35b6872fc
var500 <varun5chodha@gmail.com>
2024-11-06 16:41:25 +0530

    Pointers

--- tutorial/passByReference/main.go
+++ tutorial/passByReference/main.go
@@ -4,37 +4,44 @@ package main
 
 import "fmt"
 
-func updateName(n string) string {
-	n = "Mario"
-	return n
+func updateName(n *string) {
+	*n = "Mario"
 }
 
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
-}
+// func updateMenu(y map[string]float64) {
+// 	y["coffee"] = 2.99
+// }
 
 func main() {
 	name := "Varun"
 
-	name = updateName(name)
+	// name = updateName(name)
 
-	fmt.Println(name)
+	// fmt.Println(name)
 
-	// Group A: int, string, bool. floats, arrays, structs
+	// Group A: Non-Pointer Values
+	// type -> int, string, bool. floats, arrays, structs
 	// These data types are handled as pass my value
 
-	//Group B:
+	//Group B: Pointer Wrapper Values
 	// type -> slices, map, functions
 	// These datatypes are handled as pass by reference where the copy of variable points to the same location where data is stored
 
-	menu := map[string]float64{
-		"pie":     8.99,
-		"chicken": 12.99,
-	}
+	// menu := map[string]float64{
+	// 	"pie":     8.99,
+	// 	"chicken": 12.99,
+	// }
 
-	fmt.Println(menu)
+	// fmt.Println(menu)
 
-	updateMenu(menu)
+	// updateMenu(menu)
 
-	fmt.Println(menu)
+	// fmt.Println(menu)
+
+	// Pointers can we used like this
+
+	m := &name
+	fmt.Println(name)
+	updateName(m)
+	fmt.Println(name)
 }
